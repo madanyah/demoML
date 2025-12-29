cc1d4d0a8637f5d32190b2bb932bfbfccf194447
Rahmahdaniah Fitri <11231084@student.itk.ac.id>
2025-12-29 09:48:40 +0800

    bismillah ram nya ok

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
+import torch 
 import librosa
 import tempfile
 
@@ -17,12 +18,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-print("🔄 Loading emotion detection model...")
-classifier = pipeline(
-    "audio-classification",
-    model="superb/wav2vec2-base-superb-er"
-)
-print("✅ Model loaded!")
+classifier = None
+
+def get_model():
+    global classifier
+    if classifier is None:
+        print("🔄 Loading model...")
+        classifier = pipeline(
+            "audio-classification",
+            model="superb/wav2vec2-base-superb-er"
+        )
+        print("✅ Model loaded")
+    return classifier
 
 LABEL_MAP = {
     "neutral": "neu",
@@ -58,7 +65,8 @@ async def predict(file: UploadFile):
 
         audio, sr = librosa.load(tmp_path, sr=16000, mono=True)
 
-        results = classifier(audio)
+        model = get_model()
+        results = model(audio)
         top = results[0]
 
         raw_label = top["label"].lower()
